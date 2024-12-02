124d090976d86c80de68f9bcf53eb31f1ad40f3f
lucky2301196 <lucky2301196@gmail.com>
2024-12-03 01:23:02 +0530

    Second commit autobg

---
+++ src/main/java/com/example/autobg2/templated/UserTemplated.java
@@ -0,0 +1,5 @@
+package com.example.autobg2.templated;
+
+public class UserTemplated {
+
+}
---
+++ src/main/java/com/example/autobg2/userregistration/UserEntityRegistration.java
@@ -0,0 +1,5 @@
+package com.example.autobg2.userregistration;
+
+public class UserEntityRegistration {
+
+}
