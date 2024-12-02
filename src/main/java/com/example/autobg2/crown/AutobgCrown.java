311bc9dd43ebbf688026142f56caa1d29dc74890
lucky2301196 <lucky2301196@gmail.com>
2024-12-03 01:29:46 +0530

    Third commit autobg

---
+++ src/main/java/com/example/autobg2/crown/AutobgCrown.java
@@ -0,0 +1,5 @@
+package com.example.autobg2.crown;
+
+public class AutobgCrown {
+
+}
---
+++ src/main/java/com/example/autobg2/templatedrepositiory/TemplatedRepositiry.java
@@ -0,0 +1,5 @@
+package com.example.autobg2.templatedrepositiory;
+
+public interface TemplatedRepositiry {
+
+}
